5b9d364c0651711120fbb809cea54b301bbdeb5f
Tiago Novaes <tiagousnovaes@gmail.com>
2023-01-26 18:02:08 -0300

    Code organization fine adjustments

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,6 +11,8 @@ let form = {
     levels: []
 }
 
+//MAIN
+
 getQuizz();
 
 function getQuizz() {
@@ -28,6 +30,140 @@ function getQuizz() {
         }
     });
 }
+
+//QUIZZ PAGE
+
+function chooseQuizz(id) {
+    const promise = axios.get(uri + id);
+    promise.then(renderQuizz);
+    promise.catch((error) => {
+        console.log(error);
+    });
+}
+function renderQuizz(answer) {
+    document.querySelector('main').classList.add('hide');
+    document.querySelector('.quizz-page').classList.remove('hide');
+    document.querySelector('.quizz-page').scrollIntoView({ behavior: 'smooth', block: "start", });
+
+    document.querySelector('.banner').innerHTML = `<img src="${answer.data.image}" alt="">
+            <span>${answer.data.title}</span>`;
+
+    document.querySelector('.question-feed').innerHTML = "";
+
+    numQuestions = answer.data.questions.length;
+    levelsRef = answer.data.levels;
+
+    for (let i = 0; i < numQuestions; i++) {
+        document.querySelector('.question-feed').innerHTML += `
+            <div class="question-box">
+                <div class="question" style="background-color:${answer.data.questions[i].color};">
+                    <span>${answer.data.questions[i].title}</span>
+                </div>
+                <div class="answer-box" id="data${i}"></div>
+            </div>`;
+    }
+
+
+    for (let j = 0; j < numQuestions; j++) {
+        const sortedAnswers = answer.data.questions[j].answers.sort(shuffle);
+        for (let z = 0; z < sortedAnswers.length; z++) {
+            if (sortedAnswers[z].isCorrectAnswer) {
+                document.getElementById(`data${j}`).innerHTML += `
+                <div class="answer" onclick="answerSelection(this, ${z});">
+                    <img src="${sortedAnswers[z].image}" alt="">
+                    <span class="v">${sortedAnswers[z].text}</span>
+                </div>`;
+            } else {
+                document.getElementById(`data${j}`).innerHTML += `
+                <div class="answer" onclick="answerSelection(this, ${z});">
+                    <img src="${sortedAnswers[z].image}" alt="">
+                    <span class="f">${sortedAnswers[z].text}</span>
+                </div>`;
+            }
+        }
+    }
+}
+
+function shuffle() {
+    return Math.random() - 0.5;
+}
+
+function answerSelection(item, index) {
+    if (item.parentElement.classList.contains('answered')) {
+        return;
+    } else {
+        questionsRef++;
+        item.parentElement.classList.add('answered')
+        item.classList.add('selection')
+        const answerRefPos = item.parentElement.id.replace('data', '');
+        console.log(answerRefPos)
+        console.log(index)
+        if (item.querySelector('span').classList.contains('v')) {
+            points++;
+        }
+        setTimeout(() => {
+            smoothMove(answerRefPos);
+        }, 2000);
+        if (questionsRef === numQuestions) {
+            setTimeout(() => {
+                calculateResults(points, numQuestions);
+            }, 2000);
+        }
+    }
+}
+
+function smoothMove(lastId) {
+    console.log(lastId);
+    console.log(`data${Number(lastId) + 1}`)
+    if (document.getElementById(`data${Number(lastId) + 1}`) === null) {
+        return;
+    }
+    document.getElementById(`data${Number(lastId) + 1}`).parentElement.scrollIntoView({ behavior: 'smooth', block: "center", })
+}
+
+function calculateResults(points, numQuestions) {
+    document.querySelector('.quizz-result').classList.remove('hide');
+    document.querySelector('.final').classList.remove('hide');
+    let score = Math.floor((points / numQuestions) * 100);
+    console.log(levelsRef);
+    for (let i = (levelsRef.length - 1); i >= 0; i--) {
+        if (score >= levelsRef[i].minValue) {
+            document.querySelector('.quizz-result').innerHTML = `
+                <div class="result-title">
+                    <span>${score}% de acerto: ${levelsRef[i].title}</span>
+                </div>
+                <div class="result">
+                    <img src=${levelsRef[i].image} alt="">
+                    <span>${levelsRef[i].text}</span>
+                </div>
+            `;
+            document.querySelector('.quizz-result').scrollIntoView({ behavior: 'smooth', block: "center", });
+            return;
+        }
+    }
+}
+
+function restartQuizz() {
+
+    let restartQuestions = document.querySelectorAll('.answer-box');
+    for (let i = 0; i < restartQuestions.length; i++) {
+        restartQuestions[i].classList.remove("answered");
+        restartQuestions[i].querySelector('.selection').classList.remove("selection");
+    }
+    points = 0;
+    questionsRef = 0;
+    document.querySelector('.quizz-result').classList.add('hide');
+    document.querySelector('.final').classList.add('hide');
+    document.querySelector('.quizz-page').scrollIntoView({ behavior: 'smooth', block: "start", });
+
+}
+
+function goHome() {
+    document.querySelector('.quizz-page').classList.add('hide');
+    window.location.reload();
+}
+
+//CREATE QUIZZ
 
 function creationPage() {
     document.querySelector('main').classList.add('hide');
@@ -81,155 +217,25 @@ function inputChecks(passCheck) {
     }
 }
 
-function chooseQuizz(id) {
-    const promise = axios.get(uri + id);
-    promise.then(renderQuizz);
-    promise.catch((error) => {
-        console.log(error);
-    });
-}
-function renderQuizz(answer) {
-    document.querySelector('main').classList.add('hide');
-    document.querySelector('.quizz-page').classList.remove('hide');
-    document.querySelector('.quizz-page').scrollIntoView({behavior:'smooth', block: "start",});
-
-    document.querySelector('.banner').innerHTML = `<img src="${answer.data.image}" alt="">
-            <span>${answer.data.title}</span>`;
-
-    document.querySelector('.question-feed').innerHTML = "";
-
-    numQuestions = answer.data.questions.length;
-    levelsRef = answer.data.levels;
-
-    for (let i = 0; i < numQuestions; i++) {
-        document.querySelector('.question-feed').innerHTML += `
-            <div class="question-box">
-                <div class="question" style="background-color:${answer.data.questions[i].color};">
-                    <span>${answer.data.questions[i].title}</span>
-                </div>
-                <div class="answer-box" id="data${i}"></div>
-            </div>`;
-    }
-
-
-    for (let j = 0; j < numQuestions; j++) {
-        const sortedAnswers = answer.data.questions[j].answers.sort(shuffle);
-        for (let z = 0; z < sortedAnswers.length; z++) {
-            if(sortedAnswers[z].isCorrectAnswer){
-                document.getElementById(`data${j}`).innerHTML += `
-                <div class="answer" onclick="answerSelection(this, ${z});">
-                    <img src="${sortedAnswers[z].image}" alt="">
-                    <span class="v">${sortedAnswers[z].text}</span>
-                </div>`;
-            } else {
-                document.getElementById(`data${j}`).innerHTML += `
-                <div class="answer" onclick="answerSelection(this, ${z});">
-                    <img src="${sortedAnswers[z].image}" alt="">
-                    <span class="f">${sortedAnswers[z].text}</span>
-                </div>`;
-            }
-        }
-    }
-}
-
-function shuffle() {
-    return Math.random() - 0.5;
-}
-
-function answerSelection(item, index) {
-    if (item.parentElement.classList.contains('answered')) {
-        return;
-    } else {
-        questionsRef++;
-        item.parentElement.classList.add('answered')
-        item.classList.add('selection')
-        const answerRefPos = item.parentElement.id.replace('data', '');
-        console.log(answerRefPos)
-        console.log(index)
-        if (item.querySelector('span').classList.contains('v')){
-            points++;
-        }
-        setTimeout(() => {
-            smoothMove(answerRefPos);
-        }, 2000);
-        if(questionsRef === numQuestions){
-            setTimeout(() => {
-                calculateResults(points, numQuestions);
-            }, 2000);
-        }
-    }
-}
-
-function smoothMove(lastId){
-    console.log(lastId);
-    console.log(`data${Number(lastId)+1}`)
-    if(document.getElementById(`data${Number(lastId)+1}`) === null){
-        return;
-    }
-    document.getElementById(`data${Number(lastId)+1}`).parentElement.scrollIntoView({behavior:'smooth', block: "center",})
-}
-
-function calculateResults(points, numQuestions){
-    document.querySelector('.quizz-result').classList.remove('hide');
-    document.querySelector('.final').classList.remove('hide');
-    let score = Math.floor((points/numQuestions)*100);
-    console.log(levelsRef);
-    for (let i = (levelsRef.length-1); i >= 0; i--){
-        if(score >= levelsRef[i].minValue){
-            document.querySelector('.quizz-result').innerHTML =`
-                <div class="result-title">
-                    <span>${score}% de acerto: ${levelsRef[i].title}</span>
-                </div>
-                <div class="result">
-                    <img src=${levelsRef[i].image} alt="">
-                    <span>${levelsRef[i].text}</span>
-                </div>
-            `;
-            document.querySelector('.quizz-result').scrollIntoView({behavior:'smooth', block: "center",});
-            return;
-        }
-    } 
-}
-
-function restartQuizz(){
-   
-    let restartQuestions = document.querySelectorAll('.answer-box');
-    for (let i = 0; i < restartQuestions.length; i++){
-        restartQuestions[i].classList.remove("answered");
-        restartQuestions[i].querySelector('.selection').classList.remove("selection");
-    }
-    points = 0;
-    questionsRef = 0;
-    document.querySelector('.quizz-result').classList.add('hide');
-    document.querySelector('.final').classList.add('hide');
-    document.querySelector('.quizz-page').scrollIntoView({behavior:'smooth', block:"start",});
-    
-}
-
-function goHome(){
-    document.querySelector('.quizz-page').classList.add('hide');
-    window.location.reload();
-}
-
 function createQuizQuestions(passCheck) {
     document.querySelector('.creation>.first').classList.add('hide');
     const pageCreation = document.querySelector('.creation>.second');
     pageCreation.classList.remove('hide');
-    
+
     pageCreation.innerHTML += `
     <div class='create-quiz-questions'>
       <h1>Crie suas perguntas</h1>
       <ul></ul>
     </div>
     `;
-    
-    for(let i = 0; i < 3; i++) {
-      const list = document.querySelector('.create-quiz-questions>ul');
-      
-      list.innerHTML += `
+
+    for (let i = 0; i < 3; i++) {
+        const list = document.querySelector('.create-quiz-questions>ul');
+
+        list.innerHTML += `
           <li>
             <p>Pergunta ${i + 1}</p>
-            <form class='questions-form-${i+1}'>
+            <form class='questions-form-${i + 1}'>
             <input class='question-title' value='xxxx' required/>
               <input class='color-title' required/>
               <p>Resposta Correta</p>
@@ -237,40 +243,39 @@ function createQuizQuestions(passCheck) {
               <input class='correct-img-answer' required type='url'/>
               <p>Resposta incorretas</p>
               <input class='wrong-answer1' required/>
-              <input class='wrong-img-answer2' required type='url'/>
+              <input class='wrong-img-answer1' required type='url'/>
+              <br>
+              <input class='wrong-answer2' />
+              <input class='wrong-img-answer2' type='url' />
               <br>
               <input class='wrong-answer3' />
               <input class='wrong-img-answer3' type='url' />
-              <br>
-              <input class='wrong-answer4' />
-              <input class='wrong-img-answer4' type='url' />
             </form>
           </li>
         `;
-  
-        const questionTitle = document.querySelector(`.questions-form-${i+1}>.question-title`).value;
-        const colorTitle = document.querySelector(`.questions-form-${i+1}>.color-title`).value;
-        const correctAnswer = document.querySelector(`.questions-form-${i+1}>.correct-answer`).value;
-        const correctImg = document.querySelector(`.questions-form-${i+1}>.correct-img-answer`).value;
-        const wrongAnswer1 = document.querySelector(`.questions-form-${i+1}>.wrong-answer1`).value;
-        const wrongImg1 = document.querySelector(`.questions-form-${i+1}>.wrong-img-answer1`).value;
-        const wrongAnswer2 = document.querySelector(`.questions-form-${i+1}>.wrong-answer2`).value;
-        const wrongImg2 = document.querySelector(`.questions-form-${i+1}>.wrong-img-answer2`).value;
-        const wrongAnswer3 = document.querySelector(`.questions-form-${i+1}>.wrong-answer3`).value;
-        const wrongImg3 = document.querySelector(`.questions-form-${i+1}>.wrong-img-answer3`).value;
-  
-        
-      }
-      
-      
-  
+
+        const questionTitle = document.querySelector(`.questions-form-${i + 1}>.question-title`).value;
+        const colorTitle = document.querySelector(`.questions-form-${i + 1}>.color-title`).value;
+        const correctAnswer = document.querySelector(`.questions-form-${i + 1}>.correct-answer`).value;
+        const correctImg = document.querySelector(`.questions-form-${i + 1}>.correct-img-answer`).value;
+        const wrongAnswer1 = document.querySelector(`.questions-form-${i + 1}>.wrong-answer1`).value;
+        const wrongImg1 = document.querySelector(`.questions-form-${i + 1}>.wrong-img-answer1`).value;
+        const wrongAnswer2 = document.querySelector(`.questions-form-${i + 1}>.wrong-answer2`).value;
+        const wrongImg2 = document.querySelector(`.questions-form-${i + 1}>.wrong-img-answer2`).value;
+        const wrongAnswer3 = document.querySelector(`.questions-form-${i + 1}>.wrong-answer3`).value;
+        const wrongImg3 = document.querySelector(`.questions-form-${i + 1}>.wrong-img-answer3`).value;
+
+
+    }
+
+
+
     pageCreation.innerHTML += `
       <button onclick="checkQuestions()">Prosseguir para criar niveis</button>
     `;
-  
-  }
-  createQuizQuestions();
-  
-  function checkQuestions() {
-    
-  }
+
+}
+
+function checkQuestions() {
+
+}
