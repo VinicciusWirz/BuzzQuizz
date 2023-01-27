1a3cbee10c672784b1644f4ecdaef8e631dab9dc
Viniccius Wirz <vinimovel23@gmail.com>
2023-01-27 19:39:34 -0300

    Changes landing page layout responsivity

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -73,6 +73,7 @@ function validateInput() {
 function inputChecks(passCheck) {
     if (passCheck.length === 4) {
         //execute command to show next step;
+        createQuizQuestions(passCheck);
     }
 }
 
