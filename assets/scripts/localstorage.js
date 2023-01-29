aeea6f163d600599366e9bb53f08f38b11fc7a68
Viniccius Wirz <vinimovel23@gmail.com>
2023-01-29 04:42:40 -0300

    Add quizz deletion functionality

--- assets/scripts/localstorage.js
+++ assets/scripts/localstorage.js
@@ -8,6 +8,10 @@ function localStorageRender() {
             const objId = Number(element);
             const objPARSE = JSON.parse(localStorage.getItem(objId))
             document.querySelector('.user-list').innerHTML += `<li onclick="chooseQuizz(${objId});">
+            <div class="localStorageBtns">
+            
+            <button class="deleteBtn" onclick="event.stopPropagation(); removeQuizz(${objId});"><ion-icon name="trash-outline"></ion-icon></button>
+            </div>
             <img src="${objPARSE.image}" alt="" onerror="this.src='./assets/imgs/error404.png'">
             <span>${objPARSE.title}</span>
         </li>`;
@@ -28,4 +32,23 @@ function postQuestion() {
         localStorage.setItem(answerID, sendingObj);
         renderSucess(answerID);
     })
+}
+
+function removeQuizz(quizzId) {
+    let config = {
+        headers: {
+            'Secret-Key': ''
+        }
+    };
+    const confirmation = confirm('Você quer mesmo deletar este quizz?');
+    if (confirmation === true) {
+        document.querySelector('.loadingscreen').classList.remove('hide');
+        const key = JSON.parse(localStorage.getItem(quizzId)).key;
+        config.headers["Secret-Key"] = key;
+        const promise = axios.delete(uri + quizzId, config);
+        promise.then(() => {
+            localStorage.removeItem(quizzId);
+            goHome();
+        });
+    }
 }
